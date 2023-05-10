86e316868ae14566b5face15a190b3909a73e031
Jens Klingelhoefer <j.klingelhoefer@sap.com>
2023-05-10 06:04:29 +0000

    adding shell script with cf login

---
+++ cf_login_with_origin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cf api api.cf.eu10.hana.ondemand.com
+cf login --origin ajsk93daf-platform -o "SAP Internal - P&T CEE_de-dmi-eu10-p80l2l5x" -s carl-zeiss -u klingelhoefer.jens@gmail.com
